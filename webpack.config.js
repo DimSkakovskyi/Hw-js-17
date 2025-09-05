3017feb020956ff9cc5d974e538916cf5bb8c835
DimSkakovskyi <dimonskak@ukr.net>
2025-09-05 20:45:21 +0300

    Beggining of Webpack Advanced

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { default: test } = require('node:test');
-const { useFunc } = require('ajv/dist/compile/util');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -24,33 +23,43 @@ module.exports = {
       '@assets': path.resolve(__dirname, 'src/assets')
     }
   },
+  target: 'web',
+  devServer: {
+    port: 4200,
+    hot: false
+  },
   plugins: [new HtmlWebpackPlugin({
     template: './index.html',
   }
-  ), new CleanWebpackPlugin()],
+  ), new CleanWebpackPlugin(),
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: path.resolve(__dirname, 'src', 'assets', 'logo.png'), to: path.resolve(__dirname, 'dist', 'logo.png') }],
+  })],
   optimization: {
     splitChunks: {
-    },
-    module: {
-      rules: [{
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|svg|gif|webp)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[name].[hash][ext]'
-        }
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot|otf)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name].[hash][ext]'
-        }
-      }
-      ]
+      chunks: 'all',
     }
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|jpe?g|svg|gif|webp)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'assets/images/[name].[hash][ext]'
+      }
+    },
+    {
+      test: /\.(woff|woff2|ttf|eot|otf)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'assets/fonts/[name].[hash][ext]'
+      }
+    }
+    ]
   }
 };
